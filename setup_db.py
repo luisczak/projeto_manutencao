bd6a4ee34dbb33a82dd4e3d63e4abc31c42375f9
luisczak <ens-eduardofranczak@ugv.edu.br>
2026-04-27 21:36:17 -0300

    docs: estratégia de testes e correções no backend

--- setup_db.py
+++ setup_db.py
@@ -1,11 +1,12 @@
 import psycopg2
+import os
 
 DB_CONFIG = {
-    'dbname': 'manutencao_db',
-    'user': 'postgres',
-    'password': '123', 
-    'host': 'localhost',
-    'port': '5432'
+    'dbname': os.getenv('DB_NAME', 'manutencao_db'),
+    'user': os.getenv('DB_USER', 'postgres'),
+    'password': os.getenv('DB_PASSWORD', 'postgres'),
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'port': os.getenv('DB_PORT', '5432')
 }
 
 def criar_tabela():
@@ -15,8 +16,8 @@ def criar_tabela():
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS requisicoes (
                 codigo VARCHAR(10) PRIMARY KEY,
-                descricao TEXT NOT NULL,
-                acao_realizada TEXT,
+                descricao TEXT NOT NULL CHECK (descricao <> ''),
+                acao_realizada TEXT NOT NULL CHECK (acao_realizada <> ''),
                 data VARCHAR(15) NOT NULL,
                 hora VARCHAR(15) NOT NULL
             )
